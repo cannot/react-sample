c6103bcccfa2e5c1b0fc0abb767f562ef8ffa40f
J.U.I.S.S <cr7rn10@gmail.com>
2017-09-13 19:41:32 +0700

    add js

---
+++ src/components/Viewlist-replica.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Viewlist_replica = () => (
+  <div>
+    <h1>This is Viewlist-replica</h1>
+  </div>
+)
+
+export default Viewlist_replica
---
+++ src/components/Viewlist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Viewlist = () => (
+  <div>
+    <h1>This is Viewlist</h1>
+  </div>
+)
+
+export default Viewlist
